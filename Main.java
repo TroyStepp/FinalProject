f9da67916a102ab15232ed05e69298668944b118
DSM-Salas <diegomldz4@gmail.com>
2025-12-04 10:30:37 -0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         JFrame Frame = new JFrame(" Coffee Shop");
+        Frame.setSize(400, 200);
 
-         JLabel label = new Jlabel("")
+    }
+}
