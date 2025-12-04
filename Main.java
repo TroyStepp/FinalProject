df1f7c671dfae3caaaf4dbd1ed0c5516f0ed812d
DSM-Salas <diegomldz4@gmail.com>
2025-12-04 10:28:21 -0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame Frame = new JFrame(" Coffee Shop");
+
+         JLabel label = new Jlabel("")
